a7ef8a57a78d95273758ba81e87b3b6f0c230912
Westdev20 <michmbah@gmail.com>
2023-02-19 14:55:10 -0800

    Inventing

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/**
+* main - main function entry
+*
+* Return: int(0)
+*/
+int main(void)
+{
+int i, j;
+for (i = 0; i < 9; i++)
+{
+for (j = i + 1; j < 10; j++)
+{
+putchar((char) (i + 48));
+putchar((char) (j + 48));
+if (i == 8 && j == 9)
+continue;
+putchar(',');
+putchar(' ');
+}
+}
+putchar('\n');
+return (0);
+}
