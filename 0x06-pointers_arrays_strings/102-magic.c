c45baee3c2709c4bef26c7ac12f1ca008a28cece
Westdev20 <michmbah@gmail.com>
2023-03-02 17:47:48 -0800

    0x06. C - More pointers, arrays and strings

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -1,16 +1,24 @@
 #include <stdio.h>
-int a =48;
-char* charhack;
-char chack;
-void* hackptr;
 
-int main() {
-     hackptr = &a;
-     printf("hackptr contains address of a %p\n", hackptr );
-     charhack = (char*) hackptr;
-  
-    printf("charhack contains address of hackptr %p\n", charhack );  
-     chack= *charhack;
-    printf("charhack now has character of %c", *charhack);
-    return 0;
+int main(void)
+{
+  int n;
+  int a[5];
+  int *p;
+
+  a[2] = 1024;
+  p = &n;
+  /*
+   * write your line of code here...
+   * Remember:
+   * - you are not allowed to use a
+   * - you are not allowed to modify p
+   * - only one statement
+   * - you are not allowed to code anything else than this line of code
+   */
+  *(p + 5) = 98;
+  /* ...so that this prints 98\n */
+  printf("a[2] = %d\n", a[2]);
+  return (0);
 }
+
